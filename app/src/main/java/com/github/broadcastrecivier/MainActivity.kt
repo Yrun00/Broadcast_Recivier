14160ea5c8d552c3e260e9de26e0027cd7a29d6b
Yrun00 <danil.budarenkov@gmail.com>
2026-01-01 09:09:59 +0400

    Dynamic Receiver

--- app/src/main/java/com/github/broadcastrecivier/MainActivity.kt
+++ app/src/main/java/com/github/broadcastrecivier/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.github.broadcastrecivier
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -18,10 +23,29 @@ import androidx.compose.ui.unit.sp
 import com.github.broadcastrecivier.ui.theme.BroadcastRecivierTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var tickReceiver: BroadcastReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
+
 
         Log.e("BootCompleteReceiver", "===== APP STARTED =====")
         super.onCreate(savedInstanceState)
+
+        tickReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.e("TickReceiver", "===== TIME TICK RECEIVED =====")
+                Log.d("TickReceiver", "Current time: ${System.currentTimeMillis()}")
+                Log.e("TickReceiver", "===== END =====")
+            }
+        }
+        val filter = IntentFilter(Intent.ACTION_TIME_TICK)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(tickReceiver, filter, RECEIVER_EXPORTED)
+        } else {
+            registerReceiver(tickReceiver, filter)
+        }
+
+        Log.e("MainActivity", "Receiver registered!")
+
         setContent {
             Box(
                 modifier = Modifier.fillMaxSize(),
